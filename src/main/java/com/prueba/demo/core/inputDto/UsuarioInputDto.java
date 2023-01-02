68d327bf548b46f13981cc31b5c2f0dd7ebdbbc3
Carlos <ctangcontreras@gmail.com>
2023-01-02 16:13:21 -0500

    inputDto

---
+++ src/main/java/com/prueba/demo/core/inputDto/UsuarioInputDto.java
@@ -0,0 +1,10 @@
+package com.prueba.demo.core.inputDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UsuarioInputDto {
+    public String idUsuario;
+    private String password;
+}
