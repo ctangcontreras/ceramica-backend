e45063296c98e5d33c6b3152ef6d2f1f8cb2b257
Carlos <ctangcontreras@gmail.com>
2023-01-25 12:15:44 -0500

    reporte producto inicial y producto terminado

---
+++ src/main/java/com/prueba/demo/core/model/ReporteProductoTerminado.java
@@ -0,0 +1,39 @@
+package com.prueba.demo.core.model;
+
+
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ReporteProductoTerminado {
+    
+    private String fechaInicio;
+    private String fechaFin;
+    private String logo;
+    private List<ListaProductoTerminado> lista;
+
+    @Setter
+    @Getter
+    public static class ListaProductoTerminado {
+        private String codigo;
+        private String horno;
+        private String fechaRegistro;
+        private String paquete;
+        private List<Contenido> contenido;
+    }
+
+    @Setter
+    @Getter
+    public static class Contenido {
+
+        private String cantidadTotal;
+        private String tipoLadrillo;
+        private String estadoLadrillo;
+    }
+    
+
+}
